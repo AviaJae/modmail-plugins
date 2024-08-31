b45cb40aeb6eacd0393a05242867aea20e48c630
AviaJae <81942396+AviaJae@users.noreply.github.com>
2024-08-31 13:32:44 +0800

    Update say.py

--- say/say.py
+++ say/say.py
@@ -1,13 +1,12 @@
 from discord.ext import commands
 
-
 class Say(commands.Cog):
     """A simple say command that removes @everyone/@here mentions."""
 
     def __init__(self, bot):
         self.bot = bot
 
-    @checks.has_permissions(PermissionLevel.ADMIN)
+    @commands.has_permissions(administrator=True)
     @commands.command(name='say2')
     async def say2(self, ctx, *, message: str):
         """Sends a message, but removes @everyone and @here mentions."""
@@ -15,12 +14,19 @@ class Say(commands.Cog):
         filtered_message = message.replace("@everyone", "@\u200beveryone").replace("@here", "@\u200bhere")
         await ctx.send(filtered_message)
         
-        # Attempt to delete the command message if bot has permissions
+        # Attempt to delete the command message if the bot has permissions
         try:
             await ctx.message.delete()
         except discord.Forbidden:
             pass  # Ignore if bot doesn't have permission to delete
 
+    @say2.error
+    async def say2_error(self, ctx, error):
+        """Handles errors for the say2 command."""
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send("You do not have permission to use this command.", delete_after=10)
+        else:
+            raise error
 
 async def setup(bot):
     """Sets up the Say cog."""
